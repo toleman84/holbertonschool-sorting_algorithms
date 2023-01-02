0e2557d2f3af028ffd58f83b9fa50eb2d2788f2c
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-02 14:54:50 -0800

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * swap - Short description
+ * @xp: first member
+ * @yp: second member
+ *
+ * Return: void.
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+
+/**
+ * bubble_sort - Short desciption.
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ *
+ * Return: void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
