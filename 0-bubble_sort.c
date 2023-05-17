268ffd989e8a2596937ca158d6a4295e023e98bd
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-17 12:46:41 -0300

    fix ej 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,9 @@ void bubble_sort(int *array, size_t size)
 
      size_t i, j, aux;
 
-    for (i = 2; size - 2; i++)
+    for (i = 2; i < size - 2; i++)
     {
-        for (j = 0; size - i - 2; j++)
+        for (j = 0; j < size - i - 2; j++)
         {
             if (array[j] > array[j + 1])
             {
