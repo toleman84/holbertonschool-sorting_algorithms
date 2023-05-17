b7704f286d1e6f3f2c55b22011bf1909e51e8c20
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-17 14:47:02 -0300

    add ej 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,8 +1,31 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array in ascending order using the Bubble sort
+ * selection_sort - sorts an array of integers in ascending order using the Selection sort algorithm
  *
  * @array: The array to be sort
  * @size: Number of elements in array
  */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, aux, min_idx;
+
+    for (i = 0; i < size; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < size; j++)
+        {
+            if (array[j] < array[j - 1])
+            {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i)
+        {
+            aux = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = aux;
+        }
+        print_array(array, size);
+    }
+}
