3ebb1b364bf1c8462cef5f0e484075c1077223ac
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-17 23:32:20 -0300

    fix sort header

--- sort.h
+++ sort.h
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
-//void print_list(const listint_t *list);
+/*void print_list(const listint_t *list);*/
 
 void bubble_sort(int *array, size_t size);
-//void insertion_sort_list(listint_t **list);
+/*void insertion_sort_list(listint_t **list);*/
 void selection_sort(int *array, size_t size);
-//void quick_sort(int *array, size_t size);
+/*void quick_sort(int *array, size_t size);*/
 
 /**
  * struct listint_s - Doubly linked list node
