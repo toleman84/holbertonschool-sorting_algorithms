0eff975596b64a19067cf54956ac0b37398f087d
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-20 22:36:44 -0300

    fix ej 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,10 +10,10 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, aux, min_idx;
 
-	for (i = 0; i < array[size - 1]; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i + 1; j < array[size]; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
