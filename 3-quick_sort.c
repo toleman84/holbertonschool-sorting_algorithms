e725540dd37d2018f744805615263cdaebfa03a9
20Emi <iop.emilysanchez1@gmail.com>
2023-05-25 10:13:43 -0700

    completed task

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,72 +1,76 @@
 #include "sort.h"
 
 /**
- * 
- * 
-*/
+ *
+ *
+ */
 
 void swap(int *array, int a, int b, int size)
 {
+	int temp;
 
-    int temp = array[a];
-    array[a] = array[b];
-    array[b] = temp;
-    print_array(array, size);
+	if (array[a] != array[b])
+	{
+		temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+		print_array(array, size);
+	}
 }
 
-/**
- * 
-*/
-int Lomuto(int *array, int boy, int large, int size)
-{
-    int pivot;
-    int x, a = boy;
+	/**
+	 *
+	 */
+	int Lomuto(int *array, int boy, int large, int size)
+	{
+		int pivot;
+		int x, a = boy;
 
-    pivot = array[large];
+		pivot = array[large];
 
-    for (x = boy; x < large; x++)
-    {
-        if (array[x] <= pivot)
-        {
-            swap(array, a, x, size);
-            a++;
-        }
-    }
-    swap(array, a, x, size);
+		for (x = boy; x < large; x++)
+		{
+			if (array[x] <= pivot)
+			{
+				swap(array, a, x, size);
+				a++;
+			}
+		}
+		swap(array, a, x, size);
 
-    return (a);
-}
+		return (a);
+	}
 
-/**
- * 
-*/
+	/**
+	 *
+	 */
 
-void recursion(int array[], int boy, int large, int size)
-{
-    int pivot_i;
+	void recursion(int array[], int boy, int large, int size)
+	{
+		int pivot_i;
 
-    if (boy < large)
-    {
-        pivot_i = Lomuto(array, boy, large, size);
-        
-        recursion(array, boy, pivot_i - 1, size);
-        recursion(array, pivot_i + 1, large, size);
-    }
-}
+		if (boy < large)
+		{
+			pivot_i = Lomuto(array, boy, large, size);
 
-/**
- * 
-*/
+			recursion(array, boy, pivot_i - 1, size);
+			recursion(array, pivot_i + 1, large, size);
+		}
+	}
 
-void quick_sort(int *array, size_t size)
-{
-    size_t a = 0;
+	/**
+	 *
+	 */
 
-    for (; a < size; a++)
-    {
-        printf("%d ", array[a]);
-    }
-    printf("\n");
+	void quick_sort(int *array, size_t size)
+	{
+		size_t a = 0;
 
-    recursion(array, 0, size - 1, size);
-}
+		for (; a < size; a++)
+		{
+			printf("%d ", array[a]);
+		}
+		printf("\n");
+
+		recursion(array, 0, size - 1, size);
+	}
